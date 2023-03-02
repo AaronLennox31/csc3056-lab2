63d9b3ec625b4f76bf30e985650f2622c14c5f94
joneill95 <124259968+joneill95@users.noreply.github.com>
2023-03-02 12:48:42 +0000

    added some range tests

--- JFreeChart v1.0/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart v1.0/src/org/jfree/data/test/RangeTest.java
@@ -31,5 +31,38 @@ public class RangeTest extends TestCase {
 		assertEquals("The central value of -1 and 1 should be 0",
 				0, rangeObjectUnderTest.getCentralValue(), 0.000000001d);
 	}
+	
+	
+	//Tests For getLengthMethod
+	public void testGetLengthBothPositiveAndEqual() {
+	    Range r1 = new Range(2, 2);
+	    assertEquals("getLength: Did not return the expected output", 0.0, r1.getLength());
+	}
+	
+	public void testGetLengthBothPositiveAndNotEqual() {
+	    Range r2 = new Range(4, 9);
+	}
+	public void testGetLengthBothNegativeAndEqual() {
+	    Range r3 = new Range(-99, -99);
+	    assertEquals("getLength: Did not return the expected output", 0.0, r3.getLength());
+	}
+	public void testGetLengthBothNegativeAndNotEqual() {
+	    Range r4 = new Range(-11, -4);
+	    assertEquals("getLength: Did not return the expected output", 7.0, r4.getLength());
+	}
+	public void testGetLengthOnePositiveOneNegative() {
+	    Range r5 = new Range(-5, 3);
+	    assertEquals("getLength: Did not return the expected output", 8.0, r5.getLength());
+	}
+	
+	
+	
+	//Tests for getCentralValue
+	public void testGetCentralValue() {
+	    Range r1 = new Range(1, 5);
+	    assertEquals( 3.0, r1.getCentralValue());
+	}
+
+	
 
 }
