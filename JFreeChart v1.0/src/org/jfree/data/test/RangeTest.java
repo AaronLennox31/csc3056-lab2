9609b8269ff13953575c0b5e3c9141c02d3e9ad0
AaronLennox31 <alennox05@qub.ac.uk>
2023-03-07 21:33:25 +0000

    Add upper and lower bounds tests

--- JFreeChart v1.0/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart v1.0/src/org/jfree/data/test/RangeTest.java
@@ -62,6 +62,61 @@ public class RangeTest extends TestCase {
 	    Range r1 = new Range(1, 5);
 	    assertEquals( 3.0, r1.getCentralValue());
 	}
+	
+	
+	//Tests for getUpperBound
+	public void testGetUpperBoundBothPositiveAndEqual() {
+		Range r1 = new Range(5, 5);
+		assertEquals("getUpperBound: Did not return the expected output", 5.0, r1.getUpperBound());
+	}
+	
+	public void testGetUpperBoundBothPostiveAndNotEqual() {
+		Range r2 = new Range(1, 10);
+		assertEquals("getUpperBound: Did not return the expected output", 10.0, r2.getUpperBound());
+	}
+	
+	public void testGetUpperBoundBothNegativeAndNotEqual() {
+		Range r3 = new Range(-10, -1);
+		assertEquals("getUpperBound: Did not return the expected output", -1.0, r3.getUpperBound());
+	}
+	
+	public void testGetUpperBoundBothNegativeAndEqual() {
+		Range r4 = new Range(-5, -5);
+		assertEquals("getUpperBound: Did not return the expected output", -5.0, r4.getUpperBound());
+	}
+	
+	public void testGetUpperBoundLowerNegativeAndUpperPositive() {
+		Range r5 = new Range(-5, 5);
+		assertEquals("getUpperBound: Did not return the expected output", 5.0, r5.getUpperBound());
+	}
+	
+	//Tests for getLowerBound
+		public void testGetLowerBoundBothPositiveAndEqual() {
+			Range r1 = new Range(5, 5);
+			assertEquals("getLowerBound: Did not return the expected output", 5.0, r1.getLowerBound());
+		}
+		
+		public void testGetLowerBoundBothPostiveAndNotEqual() {
+			Range r2 = new Range(1, 10);
+			assertEquals("getLowerBound: Did not return the expected output", 1.0, r2.getLowerBound());
+		}
+		
+		public void testGetLowerBoundBothNegativeAndNotEqual() {
+			Range r3 = new Range(-10, -1);
+			assertEquals("getLowerBound: Did not return the expected output", -10.0, r3.getLowerBound());
+		}
+		
+		public void testGetLowerBoundBothNegativeAndEqual() {
+			Range r4 = new Range(-5, -5);
+			assertEquals("getLowerBound: Did not return the expected output", -5.0, r4.getLowerBound());
+		}
+		
+		public void testGetLowerBoundLowerNegativeAndUpperPositive() {
+			Range r5 = new Range(-5, 5);
+			assertEquals("getLowerBound: Did not return the expected output", -5.0, r5.getLowerBound());
+		}
+	
+		
 
 	
 
